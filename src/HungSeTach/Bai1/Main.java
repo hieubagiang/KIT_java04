02aa7a4275f060a63cd09952cf2adf928f8d0b1d
Pham Doan Hieu <hieubagiang@yahoo.com.vn>
2020-10-24 01:26:14 +0700

    fix code

---
+++ src/HungSeTach/Bai1/Main.java
@@ -0,0 +1,12 @@
+package HungSeTach.Bai1;
+
+public class Main {
+    public static void main(String[] args) {
+        SoPhuc soPhuc = new SoPhuc();
+
+        soPhuc.nhap();
+        soPhuc.xuat();
+        soPhuc.cong();
+        soPhuc.tru();
+    }
+}
